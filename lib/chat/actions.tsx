9bb65d6d2eec344b05a290d2ec30b9a588672299
Nicole Qu <nicolequ@nicoles-air-3.dyndns.rice.edu>
2024-04-16 16:55:28 -0500

    Version 1.0

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -1,22 +1,22 @@
 import 'server-only'
 
 import {
-  createAI,
-  createStreamableUI,
-  getMutableAIState,
-  getAIState,
-  render,
-  createStreamableValue
+    createAI,
+    createStreamableUI,
+    getMutableAIState,
+    getAIState,
+    render,
+    createStreamableValue
 } from 'ai/rsc'
 import OpenAI from 'openai'
 
 import {
-  spinner,
-  BotCard,
-  BotMessage,
-  SystemMessage,
-  Stock,
-  Purchase
+    spinner,
+    BotCard,
+    BotMessage,
+    SystemMessage,
+    Stock,
+    Purchase
 } from '@/components/stocks'
 
 import { z } from 'zod'
@@ -25,249 +25,255 @@ import { Events } from '@/components/stocks/events'
 import { StocksSkeleton } from '@/components/stocks/stocks-skeleton'
 import { Stocks } from '@/components/stocks/stocks'
 import {
-  formatNumber,
-  runAsyncFnWithoutBlocking,
-  sleep,
-  nanoid
+    formatNumber,
+    runAsyncFnWithoutBlocking,
+    sleep,
+    nanoid
 } from '@/lib/utils'
 import { saveChat } from '@/app/actions'
 import { SpinnerMessage, UserMessage } from '@/components/stocks/message'
 import { Chat } from '@/lib/types'
 import { auth } from '@/auth'
 
-import { querySolr } from '@/lib/chat/solr-request'; 
+import { querySolr } from '@/lib/chat/solr-request';
+import { queryGPT } from '@/lib/chat/gpt-request';
+
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || ''
+    apiKey: process.env.OPENAI_API_KEY || ''
 })
 
 async function submitUserMessage(content: string) {
-  'use server'
-  const solr_data = await querySolr("http://3.15.14.174:8983/solr/programInfo/select?q=%22astronomy%22&rows=1");
-  const solr_data_string = solr_data.toString();
-  const aiState = getMutableAIState<typeof AI>()
+    'use server'
+    const solr_request_url = await queryGPT(content);
+    // console.log(solr_request_url);
+    const solr_data = await querySolr(solr_request_url);
+    const solr_data_string = solr_data.toString();
+    // console.log(solr_data_string);
 
-  // remember the messages here
-  aiState.update({
-    ...aiState.get(),
-    messages: [
-      ...aiState.get().messages,
-      {
-        id: nanoid(),
-        role: 'system',
-        content: solr_data_string
-      },
-      {
-        id: nanoid(),
-        role: 'user',
-        content: content
-      }
-    ]
-  })
+    const aiState = getMutableAIState<typeof AI>()
 
-  let textStream: undefined | ReturnType<typeof createStreamableValue<string>>
-  let textNode: undefined | React.ReactNode
+    // remember the messages here
+    aiState.update({
+        ...aiState.get(),
+        messages: [
+            ...aiState.get().messages,
+            {
+                id: nanoid(),
+                role: 'system',
+                content: solr_data_string
+            },
+            {
+                id: nanoid(),
+                role: 'user',
+                content: content
+            }
+        ]
+    })
 
-  const ui = render({
-    model: 'gpt-3.5-turbo',
-    provider: openai,
-    initial: <SpinnerMessage />,
-    messages: [
-      {
-        role: 'system',
-        content: `\
+    let textStream: undefined | ReturnType<typeof createStreamableValue<string>>
+    let textNode: undefined | React.ReactNode
+
+    const ui = render({
+        model: 'gpt-3.5-turbo',
+        provider: openai,
+        initial: <SpinnerMessage />,
+        messages: [
+            {
+                role: 'system',
+                content: `\
 You are a Rice University courses and programs conversation bot and you can help users select the courses and programs based on their personal requirements, step by step.
 You and the user can discuss the user's academic path. 
 You need to stick to the provided related info. Output the info again with user-friendly format.
 If the info does not mention, just say you need more information or ask the user to change the question.
 `
-// Messages inside [] means that it's a UI element. For example:
-// - "[Prerequisite of COMP 631]" means that a network of the prerequisites of COMP 631 is shown to the user.
+                // Messages inside [] means that it's a UI element. For example:
+                // - "[Prerequisite of COMP 631]" means that a network of the prerequisites of COMP 631 is shown to the user.
 
-// If the user requests purchasing a stock, call \`show_stock_purchase_ui\` to show the purchase UI.
-// If the user just wants the price, call \`show_stock_price\` to show the price.
-      },
-      // provide the context (chat history)
-      ...aiState.get().messages.map((message: any) => ({
-        role: message.role,
-        content: message.content,
-        name: message.name
-      }))
-    ],
-    text: ({ content, done, delta }) => {
-      if (!textStream) {
-        textStream = createStreamableValue('')
-        textNode = <BotMessage content={textStream.value} />
-      }
-
-      if (done) {
-        textStream.done()
-        aiState.done({
-          ...aiState.get(),
-          messages: [
-            ...aiState.get().messages,
-            {
-              id: nanoid(),
-              role: 'assistant',
-              content
+                // If the user requests purchasing a stock, call \`show_stock_purchase_ui\` to show the purchase UI.
+                // If the user just wants the price, call \`show_stock_price\` to show the price.
+            },
+            // provide the context (chat history)
+            ...aiState.get().messages.map((message: any) => ({
+                role: message.role,
+                content: message.content,
+                name: message.name
+            }))
+        ],
+        text: ({ content, done, delta }) => {
+            if (!textStream) {
+                textStream = createStreamableValue('')
+                textNode = <BotMessage content={textStream.value} />
             }
-          ]
-        })
-      } else {
-        textStream.update(delta)
-      }
 
-      return textNode
-    },
-    //functions: {
-      // showStockPrice: {
-      //   description:
-      //     'Get the current stock price of a given stock or currency. Use this to show the price to the user.',
-      //   parameters: z.object({
-      //     symbol: z
-      //       .string()
-      //       .describe(
-      //         'The name or symbol of the stock or currency. e.g. DOGE/AAPL/USD.'
-      //       ),
-      //     price: z.number().describe('The price of the stock.'),
-      //     delta: z.number().describe('The change in price of the stock')
-      //   }),
-      //   render: async function* ({ symbol, price, delta }) {
-      //     yield (
-      //       <BotCard>
-      //         <StockSkeleton />
-      //       </BotCard>
-      //     )
+            if (done) {
+                textStream.done()
+                aiState.done({
+                    ...aiState.get(),
+                    messages: [
+                        ...aiState.get().messages,
+                        {
+                            id: nanoid(),
+                            role: 'assistant',
+                            content
+                        }
+                    ]
+                })
+            } else {
+                textStream.update(delta)
+            }
 
-      //     await sleep(1000)
+            return textNode
+        },
+        //functions: {
+        // showStockPrice: {
+        //   description:
+        //     'Get the current stock price of a given stock or currency. Use this to show the price to the user.',
+        //   parameters: z.object({
+        //     symbol: z
+        //       .string()
+        //       .describe(
+        //         'The name or symbol of the stock or currency. e.g. DOGE/AAPL/USD.'
+        //       ),
+        //     price: z.number().describe('The price of the stock.'),
+        //     delta: z.number().describe('The change in price of the stock')
+        //   }),
+        //   render: async function* ({ symbol, price, delta }) {
+        //     yield (
+        //       <BotCard>
+        //         <StockSkeleton />
+        //       </BotCard>
+        //     )
 
-      //     aiState.done({
-      //       ...aiState.get(),
-      //       messages: [
-      //         ...aiState.get().messages,
-      //         {
-      //           id: nanoid(),
-      //           role: 'function',
-      //           name: 'showStockPrice',
-      //           content: JSON.stringify({ symbol, price, delta })
-      //         }
-      //       ]
-      //     })
+        //     await sleep(1000)
 
-      //     return (
-      //       <BotCard>
-      //         <Stock props={{ symbol, price, delta }} />
-      //       </BotCard>
-      //     )
-      //   }
-      // },
-      
-    //}
-  })
+        //     aiState.done({
+        //       ...aiState.get(),
+        //       messages: [
+        //         ...aiState.get().messages,
+        //         {
+        //           id: nanoid(),
+        //           role: 'function',
+        //           name: 'showStockPrice',
+        //           content: JSON.stringify({ symbol, price, delta })
+        //         }
+        //       ]
+        //     })
 
-  return {
-    id: nanoid(),
-    display: ui
-  }
+        //     return (
+        //       <BotCard>
+        //         <Stock props={{ symbol, price, delta }} />
+        //       </BotCard>
+        //     )
+        //   }
+        // },
+
+        //}
+    })
+
+    return {
+        id: nanoid(),
+        display: ui
+    }
 }
 
 export type Message = {
-  role: 'user' | 'assistant' | 'system' | 'function'| 'data' | 'tool'
-  content: string
-  id: string
-  name?: string
+    role: 'user' | 'assistant' | 'system' | 'function' | 'data' | 'tool'
+    content: string
+    id: string
+    name?: string
 }
 
 export type AIState = {
-  chatId: string
-  messages: Message[]
+    chatId: string
+    messages: Message[]
 }
 
 export type UIState = {
-  id: string
-  display: React.ReactNode
+    id: string
+    display: React.ReactNode
 }[]
 
 export const AI = createAI<AIState, UIState>({
-  actions: {
-    submitUserMessage,
-    // confirmPurchase
-  },
-  initialUIState: [],
-  initialAIState: { chatId: nanoid(), messages: [] },
-  unstable_onGetUIState: async () => {
-    'use server'
+    actions: {
+        submitUserMessage,
+        // confirmPurchase
+    },
+    initialUIState: [],
+    initialAIState: { chatId: nanoid(), messages: [] },
+    unstable_onGetUIState: async () => {
+        'use server'
 
-    const session = await auth()
+        const session = await auth()
 
-    if (session && session.user) {
-      const aiState = getAIState()
+        if (session && session.user) {
+            const aiState = getAIState()
 
-      if (aiState) {
-        const uiState = getUIStateFromAIState(aiState)
-        return uiState
-      }
-    } else {
-      return
+            if (aiState) {
+                const uiState = getUIStateFromAIState(aiState)
+                return uiState
+            }
+        } else {
+            return
+        }
+    },
+    unstable_onSetAIState: async ({ state, done }) => {
+        'use server'
+
+        const session = await auth()
+
+        if (session && session.user) {
+            const { chatId, messages } = state
+
+            const createdAt = new Date()
+            const userId = session.user.id as string
+            const path = `/chat/${chatId}`
+            const title = messages[0].content.substring(0, 100)
+
+            const chat: Chat = {
+                id: chatId,
+                title,
+                userId,
+                createdAt,
+                messages,
+                path
+            }
+
+            await saveChat(chat)
+        } else {
+            return
+        }
     }
-  },
-  unstable_onSetAIState: async ({ state, done }) => {
-    'use server'
-
-    const session = await auth()
-
-    if (session && session.user) {
-      const { chatId, messages } = state
-
-      const createdAt = new Date()
-      const userId = session.user.id as string
-      const path = `/chat/${chatId}`
-      const title = messages[0].content.substring(0, 100)
-
-      const chat: Chat = {
-        id: chatId,
-        title,
-        userId,
-        createdAt,
-        messages,
-        path
-      }
-
-      await saveChat(chat)
-    } else {
-      return
-    }
-  }
 })
 
 export const getUIStateFromAIState = (aiState: Chat) => {
-  return aiState.messages
-    .filter(message => message.role !== 'system')
-    .map((message, index) => ({
-      id: `${aiState.chatId}-${index}`,
-      display:
-        message.role === 'function' ? (
-          message.name === 'listStocks' ? (
-            <BotCard>
-              <Stocks props={JSON.parse(message.content)} />
-            </BotCard>
-          ) : message.name === 'showStockPrice' ? (
-            <BotCard>
-              <Stock props={JSON.parse(message.content)} />
-            </BotCard>
-          ) : message.name === 'showStockPurchase' ? (
-            <BotCard>
-              <Purchase props={JSON.parse(message.content)} />
-            </BotCard>
-          ) : message.name === 'getEvents' ? (
-            <BotCard>
-              <Events props={JSON.parse(message.content)} />
-            </BotCard>
-          ) : null
-        ) : message.role === 'user' ? (
-          <UserMessage>{message.content}</UserMessage>
-        ) : (
-          <BotMessage content={message.content} />
-        )
-    }))
+    return aiState.messages
+        .filter(message => message.role !== 'system')
+        .map((message, index) => ({
+            id: `${aiState.chatId}-${index}`,
+            display:
+                message.role === 'function' ? (
+                    message.name === 'listStocks' ? (
+                        <BotCard>
+                            <Stocks props={JSON.parse(message.content)} />
+                        </BotCard>
+                    ) : message.name === 'showStockPrice' ? (
+                        <BotCard>
+                            <Stock props={JSON.parse(message.content)} />
+                        </BotCard>
+                    ) : message.name === 'showStockPurchase' ? (
+                        <BotCard>
+                            <Purchase props={JSON.parse(message.content)} />
+                        </BotCard>
+                    ) : message.name === 'getEvents' ? (
+                        <BotCard>
+                            <Events props={JSON.parse(message.content)} />
+                        </BotCard>
+                    ) : null
+                ) : message.role === 'user' ? (
+                    <UserMessage>{message.content}</UserMessage>
+                ) : (
+                    <BotMessage content={message.content} />
+                )
+        }))
 }
---
+++ lib/chat/gpt-request.ts
@@ -0,0 +1,51 @@
+import 'server-only';
+
+const openai_api_key = process.env.OPENAI_API_KEY || '';
+const server_ip = process.env.SERVER_IP || '';
+
+async function queryGPT(content: string): Promise<any> {
+    const prompt = `"You can use this template url to search in Solr. 
+    http://<server_ip>/solr/<core_name>/select?q=<keyword>&rows=5 
+    Now, just return a query url based on the user text, no explanation. 
+    If user asked about course, replace <core_name> by courseInfo; 
+    if about program, replace <core_name> by programInfo. 
+    Replace <keyword> with the academic topic or potential query in user text which is in between two %22. 
+    For example, "Recommend some history related courses" should replace <keyword> with %22history%22.
+    If the keyword is a course name like comp631, 
+    make sure convert the format to four character plus %20 and three digits, and then put between %22. For example: %22COMP%20631%22.
+    If you believe there are more than one keyword, put each keyword in between two %22 and concatenate them.
+    For example, keywords are A, B, C. You should replace <keyword> with %22A%22%22B%22%22C%22
+    Here is the user text: "`;
+
+    const body = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{ "role": "user", "content": prompt + content }],
+    };
+
+    const GPT_url = "https://api.openai.com/v1/chat/completions";
+
+    try {
+        const response = await fetch(GPT_url, {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + openai_api_key,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        const solr_request_url = JSON.stringify(data.choices[0].message.content);
+        //console.log("GPT request", solr_request_url);
+        const solr_request_url_modified = solr_request_url.replace("<server_ip>", server_ip).slice(1, -1);
+        //console.log("Modified GPT request", solr_request_url_modified);
+        return solr_request_url_modified;
+    } catch (error) {
+        console.error("Failed to fetch data from Solr:", error);
+        return null;
+    }
+}
+
+export { queryGPT };
